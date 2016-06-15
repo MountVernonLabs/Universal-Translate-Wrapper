9255cd58afd7d3cf737b97553ca2480be24577b7
Matt Briney <mbriney@Matts-13-MacBook-Pro.local>
2016-06-15 06:37:22 -0400

    Finalizing translation function

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 Universal PHP class the allows you to wrap text in any t() function to translate from english to a second language.
 
 
+## Installation
+1) Import /database/schema.sql to your database to create the cached table
+2) Rename config.sample.inc and set your Bing Translation API credentials and your database connection string
+
 ## Usage
 
+1) From any PHP file insert the line   
+require_once('lib/bing.translate.php');
 
+2) Call the function like so from your PHP file
+echo t("Who is George Washington?","es");
+
+The first variable is the text you want translated and the second variable is the language you'd like it translated to.  For a list of languages see the link below.
 
 ## Supported languages
 
@@ -14,4 +24,5 @@ https://msdn.microsoft.com/en-us/library/hh456380.aspx
 
 ## Thank you
 
-Bing translation class provided by Sebastian Brosch <sebastian.brosch@brosch-software.de> (c) 2012-2013, Sebastian Brosch & Brosch Software
+Bing translation class provided by Sebastian Brosch <sebastian.brosch@brosch-software.de>
+(c) 2012-2013, Sebastian Brosch & Brosch Software
--- translate.php
+++ translate.php
@@ -1,11 +1,21 @@
 <?php
-
-include('config.inc');
-require_once('lib/bing.translate.php');
-$BingTranslator = new BingTranslator($bing_app_id, $bing_secret);
-
-$translation = $BingTranslator->getTranslation('en', 'zh-CHS', 'Who is George Washington?');
-
-echo $translation."\n";
-
+function t($t,$l){
+  include('config.inc');
+  require_once('lib/bing.translate.php');
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $sql = "SELECT * FROM mv_translations WHERE language = '".$l."' AND english = '".mysqli_real_escape_string($conn,$t)."'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $translation = $row["translation"];
+    }
+  } else {
+    $BingTranslator = new BingTranslator($bing_app_id, $bing_secret);
+    $translation = $BingTranslator->getTranslation($default_lang, $l, $t);
+    $sql = "INSERT INTO mv_translations (language, english, translation) VALUES ('".$l."','".mysqli_real_escape_string($conn,$t)."','".mysqli_real_escape_string($conn,$translation)."')";
+    $conn->query($sql);
+  }
+  $conn->close();
+  return $translation;
+}
 ?>
